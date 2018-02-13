aa6342740367feca38fd6f82871ff2b97afca53a
Robert Steelheart <rob.steelheart@gmail.com>
2018-02-13 02:53:45 -0300

    basic redis cache mock

--- server.js
+++ server.js
@@ -1,42 +1,11 @@
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-const rx = require('rxjs');
 
-const axios = require('axios'); // declare axios for making http requests
-const cronBTC = rx.Observable.timer(1, 10000); //Updater BTC
-const cronETH = rx.Observable.timer(1, 8000000); //Updater ETH
-const API = 'https://www.alphavantage.co/query?';
 
-const API_KEY = 'C3DOJ8C4CTA9Z14Q';
 const router = require('./server/router')
 
-// Once connected update currency data
-const subscriberBTC = cronBTC.subscribe( () => {
-  axios.get(`${API}function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&apikey=${API_KEY}`).then(data_BTC => {
-      console.log('BTC', data_BTC.data);
-  }).catch(error => {
-      console.log('Something went wrong updating the BTC daily data', error);
-  });
-  axios.get(`${API}function=DIGITAL_CURRENCY_MONTHLY&symbol=BTC&market=USD&apikey=${API_KEY}`).then(data_BTC => {
-      console.log('BTC', data_BTC.data['Meta Data']);
-  }).catch(error => {
-      console.log('Something went wrong updating the BTC monthly data', error);
-  });
-});
 
-const subscriberETH = cronETH.subscribe( () => {
-  axios.get(`${API}function=DIGITAL_CURRENCY_DAILY&symbol=ETH&market=USD&apikey=${API_KEY}`).then(data_ETH => {
-      console.log('ETH', data_ETH.data['Meta Data']);
-  }).catch(error => {
-      console.log('Something went wrong updating the ETH daily data', error);
-  });
-  axios.get(`${API}function=DIGITAL_CURRENCY_MONTHLY&symbol=ETH&market=USD&apikey=${API_KEY}`).then(data_ETH => {
-      console.log('ETH', data_ETH.data['Meta Data']);
-  }).catch(error => {
-      console.log('Something went wrong updating the ETH monthly data', error);
-  });
-});
 
 /**
  * Get port from environment and store in Express.
--- server/router.js
+++ server/router.js
@@ -1,4 +1,55 @@
 const url = require('url');
+const rx = require('rxjs');
+const axios = require('axios'); // declare axios for making http requests
+const cronDaily = rx.Observable.timer(1, 1000000); //Updater BTC
+const cronMonthly = rx.Observable.timer(1, 8000000); //Updater ETH
+const API = 'https://www.alphavantage.co/query?';
+
+const API_KEY = 'C3DOJ8C4CTA9Z14Q';
+const redis = require("redis"), client = redis.createClient({port: 6379});
+
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
+client.on("error", function (err) {
+    console.log("Error on redis connect" + err);
+});
+
+client.on("success", () => {
+    console.log("Sucess on redis connect" + err);
+});
+// Once connected update currency data
+const subscriberDay = cronDaily.subscribe( () => {
+    axios.get(`${API}function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&apikey=${API_KEY}`).then(data_BTC => {
+        // console.log('BTC', data_BTC.data);
+        //client.setex('BTC_DAILY', 3600, data_BTC.data);
+    }).catch(error => {
+        console.log('Something went wrong updating the BTC daily data', error);
+    });
+    axios.get(`${API}function=DIGITAL_CURRENCY_DAILY&symbol=ETH&market=USD&apikey=${API_KEY}`).then(data_ETH => {
+        // console.log('ETH', data_ETH.data['Meta Data']);
+        console.log('data', JSON.stringify(data_ETH.data['Time Series (Digital Currency Daily)']))
+        client.setex('ETH_DAILY', 3600, JSON.stringify(data_ETH.data['Time Series (Digital Currency Daily)']));
+    }).catch(error => {
+        console.log('Something went wrong updating the ETH daily data', error);
+    });
+});
+
+const subscriberMonth = cronMonthly.subscribe( () => {
+    axios.get(`${API}function=DIGITAL_CURRENCY_MONTHLY&symbol=BTC&market=USD&apikey=${API_KEY}`).then(data_BTC => {
+        // console.log('BTC', data_BTC.data['Meta Data']);
+        //client.setex('BTC_MONTHLY', 86400, data_BTC.data);
+    }).catch(error => {
+        console.log('Something went wrong updating the BTC monthly data', error);
+    });
+    axios.get(`${API}function=DIGITAL_CURRENCY_MONTHLY&symbol=ETH&market=USD&apikey=${API_KEY}`).then(data_ETH => {
+        // console.log('ETH', data_ETH.data['Meta Data']);
+        //client.setex('ETH_MONTHLY', 86400, data_ETH.data);
+    }).catch(error => {
+        console.log('Something went wrong updating the ETH monthly data', error);
+    });
+});
+
 
 const headers = {
     "access-control-allow-origin": "*",
@@ -24,12 +75,15 @@ const actions = {
         const parsedUrl = url.parse(req.url);
         const endPoint = parsedUrl.pathname === '/' ? '/index.html' : parsedUrl.pathname;
 
+        client.get('ETH_DAILY', (error, result) => {
+            const data = result;
+            const statusCode = 200;
+            respond(res, data, statusCode);
+        });
 
 
-        console.log(parsedUrl, endPoint)
-        const data = JSON.stringify({hello: 'its me again'});
-        const statusCode = 200;
-        respond(res, data, statusCode);
+            console.log(parsedUrl, endPoint)
+
     }
 };
 
